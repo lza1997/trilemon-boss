2de99e0b180213bcb54ae4d89a5316db92b79bb5
edokeh <edokeh@163.com>
2013-11-03 20:40:00 +0800

    调整时间 #12

--- asset/assets/shelf/js/plan-setting/controller/distribution-controller.js
+++ asset/assets/shelf/js/plan-setting/controller/distribution-controller.js
@@ -3,12 +3,13 @@
  */
 define(function(require, exports, module) {
 
-    var DistributionController = ['$scope', 'REST', '$modal', '$routeParams', function($scope, REST, $modal, $routeParams) {
+    var DistributionController = ['$scope', 'REST', '$modal', '$routeParams', '$location', function($scope, REST, $modal, $routeParams, $location) {
         $scope.weeks = _.map('周一 周二 周三 周四 周五 周六 周日'.split(' '), function(v, index) {
             return {name: v, value: index + 1 + '', checked: false};
         });
 
         REST.PLAN_SETTING.one($routeParams.id).one('distribution').get().then(function(data) {
+            // 格式为 {1: {1:true, 2:false, ... , 23:true}, ...}
             $scope.distribution = data;
 
             // 回填周几是否被选中
@@ -17,6 +18,12 @@ define(function(require, exports, module) {
                 week.checked = _.any(v);
             });
         });
+
+        $scope.checkWeek = function(week) {
+            if (!week.checked) {
+                $scope.distribution[week.value] = {};
+            }
+        };
 
         $scope.showModal = function(week) {
             $modal.open({
@@ -30,9 +37,9 @@ define(function(require, exports, module) {
             });
         };
 
-        $scope.save = function(){
-            $scope.distribution.put().then(function(){
-                location.url('/plan-setting');
+        $scope.save = function() {
+            $scope.distribution.put().then(function() {
+                $location.url('/plan-setting');
             });
         };
 
