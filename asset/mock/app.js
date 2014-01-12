4e26df9f8d2030f7457e4ec1737be2ae1be4ed8e
edokeh <edokeh@163.com>
2014-01-12 16:09:53 +0800

    mock 启动自动打开浏览器

--- asset/mock/app.js
+++ asset/mock/app.js
@@ -30,37 +30,5 @@ app.get('/', function(req, res) {
 
 require('./controller/shelf')(app);
 
-//
-//app.get('/shelf/plan-settings', function(req, res) {
-//    var json = requireUncached('./json/shelf/plan-settings.json');
-//    json.pageNum = parseInt(req.query.page || 1);
-//    res.json(json);
-//});
-//app.get('/shelf/plan-settings/chart', function(req, res){
-//    var json = requireUncached('./json/shelf/chart.json');
-//    res.json(json);
-//});
-//app.get('/shelf/sellercats', function(req, res) {
-//    var json = requireUncached('./json/shelf/sellercats.json');
-//    res.json(json);
-//});
-//app.get('/shelf/plan-settings/:id/items', function(req, res) {
-//    var json = requireUncached('./json/shelf/items.json');
-//    json.pageNum = parseInt(req.query.page || 1);
-//    res.json(json);
-//});
-//app.get('/shelf/plan-settings/:id', function(req, res){
-//    var json = requireUncached('./json/shelf/plan-setting.json');
-//    res.json(json);
-//});
-//app.post('/shelf/plan-settings/:id/items/:nid/exclude', function(req, res){
-//    req.body.exclude = true;
-//    res.json(req.body);
-//});
-//
-//app.del('/shelf/plan-settings/:id/items/:nid/exclude', function(req, res){
-//    req.body.exclude = false;
-//    res.json(req.body);
-//});
-
 app.listen(8888);
+require("openurl").open("http://localhost:8888");
